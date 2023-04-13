4a9f63414e6d711738dab13ac345f1d1183d2f59
Valeriya <kazan_vv@mail.ru>
2023-04-13 15:35:49 +0500

    11, 12

---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Напишите программу, которая выводит случайное
+//трёхзначное число и удаляет вторую цифру этого
+//числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число {number}");
+
+int DeleteCenter (int num)
+{
+    int first = num / 100;
+    int third = num % 10;
+    int answer = first * 10 + third;
+    return answer;
+}
+Console.WriteLine($" двухзначное число {DeleteCenter(number)}");
---
+++ Task12/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли первое число
+//кратным второму. Если первое число не кратно
+//второму, то программа выводит остаток от деления.
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = Remainder (numberA, numberB);
+if (result == 0)
+{
+Console.Write("кратно");
+}
+else
+{
+Console.Write($"не кратно, остаток  {result}");
+}
+
+int Remainder(int numA, int numB)
+{
+    return numA % numB;
+}
